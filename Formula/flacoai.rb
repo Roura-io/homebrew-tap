10dfd55dc4176b33fd5c9773002f2d4065e0a1ab
Christopher J. Roura <cjroura@roura.io>
2026-04-04 12:41:24 -0400

    Update SHA256 for v0.1.0 (Ollama default)

--- Formula/flacoai.rb
+++ Formula/flacoai.rb
@@ -2,7 +2,7 @@ class Flacoai < Formula
   desc "Local AI CLI powered by Ollama"
   homepage "https://github.com/Roura-io/flaco"
   url "https://github.com/Roura-io/flaco/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "fb7a7db50c6b6ad559282643c084464e792aadce38b870868f841649f9fa8f03"
+  sha256 "3613c20e795f5243345cd3a58ceea449d034b0d9cfd60e568ab6c61848b8bd43"
   license "MIT"
 
   depends_on "rust" => :build
